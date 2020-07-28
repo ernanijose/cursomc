f8d4d4f298a8300d86e0829d0e0ff256092ff75a
ernanijose <ernanijose@gmail.com>
2020-07-28 09:53:59 -0300

    Iniciando o Projeto

---
+++ src/test/java/com/olhachei/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olhachei.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
