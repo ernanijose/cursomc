174cbc8a9cf78822704088750bf7c5878d71db1c
ernanijose <ernanijose@gmail.com>
2020-08-04 19:20:20 -0300

    Validação sintatica com Bean Validation

--- src/main/java/com/olhachei/cursomc/services/CategoriaService.java
+++ src/main/java/com/olhachei/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.olhachei.cursomc.domain.Categoria;
+import com.olhachei.cursomc.dto.CategoriaDTO;
 import com.olhachei.cursomc.repositories.CategoriaRepository;
 import com.olhachei.cursomc.services.exceptions.DataIntegratyException;
 import com.olhachei.cursomc.services.exceptions.ObjectNotFoundException;
@@ -54,6 +55,11 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto)
+	{
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 	
